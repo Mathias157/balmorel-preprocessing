284e074c6d8355c88f55166025a3d684e58ea83c
Mathias157 <mathiasros@gmail.com>
2024-03-14 11:45:34 +0100

    Made main file more main-ish

--- Raw Data Processing/main.py
+++ Raw Data Processing/main.py
@@ -22,53 +22,71 @@ if style == 'report':
 elif style == 'ppt':
     plt.style.use('dark_background')
     fc = 'none'
-    
-the_index, areas, c = prepared_geofiles('nuts3')
 
+# Go to section X. for the execution of the functions below
 
 #%% ------------------------------- ###
 ###     1. District Heating Data    ###
 ### ------------------------------- ###
 
-### 1.0 Choose spatial resolution
-choice = 'NUTS3'
-the_index, areas, c = prepared_geofiles(choice)
-
-### 1.1 Aggregate district heating data
-DKareas = areas[areas[the_index].str.find('DK') != -1]
-DH = DistrictHeat('Denmark')
-DH.dfint2 = DH.find_intersects(DKareas) # Find intersects between district heat areas and chosen areas
-DH.assign_DH(DKareas, DH.dfint)
-DH.assign_DHT(DKareas, DH.dfint)
+def generate_DH_data(areas):
+    ### 1.0 Aggregate district heating data
+    DKareas = areas[areas[the_index].str.find('DK') != -1]
+    DH = DistrictHeat('Denmark')
+    DH.dfint = calculate_intersects(DKareas, DH.geo) # Find intersects between district heat areas and chosen areas
+    DH.assign_DH(DKareas, DH.dfint)
+    DH.assign_DHT(DKareas, DH.dfint)
 
 
-### 1.2 Check that the aggregation got all data:
-# Annual DH
-print('\nOriginal data, annual DH:')
-print(DH.DH[DH.DH.A.str.find('DK') != -1].pivot_table(index='A', columns='Y').sum() / 1e6)
-print('\nNew data, annual DH:')
-print(DH.dfDH.sum() / 1e6)
+    ### 1.1 Check that the aggregation got all data:
+    # Annual DH
+    print('\nOriginal data, annual DH:')
+    print(DH.DH[DH.DH.A.str.find('DK') != -1].pivot_table(index='A', columns='Y').sum() / 1e6)
+    print('\nNew data, annual DH:')
+    print(DH.dfDH.sum() / 1e6)
 
 
-## Plot original vs aggregated data
-year = '2050'
-DH.plot_original_data(year, DKareas, plot_density=True)
-DH.plot_aggregated_data(year, DKareas, True)
+    ## Plot original vs aggregated data
+    year = '2050'
+    DH.plot_original_data(year, DKareas, plot_density=True)
+    DH.plot_aggregated_data(year, DKareas, True)
 
 
-### 1.3 Save to .incfiles
-incfiles = {}
-for file in ['DH', 'DH_VAR_T']:
-    incfiles[file] = IncFile(prefix=read_lines(file+'_prefix.inc', 
-                                              file_path='Data/IncFilePreSuf'),
-                            suffix=read_lines(file+'_suffix.inc',
-                                              file_path='Data/IncFilePreSuf'),
-                            path='Output',
-                            name=file)
+    ### 1.2 Save .inc files
+    incfiles = {}
+    for file in ['DH', 'DH_VAR_T']:
+        incfiles[file] = IncFile(prefix=read_lines(file+'_prefix.inc', 
+                                                file_path='Data/IncFilePreSuf'),
+                                suffix=read_lines(file+'_suffix.inc',
+                                                file_path='Data/IncFilePreSuf'),
+                                path='Output',
+                                name=file)
+        
+    incfiles['DH'].body = DH.dfDH.to_string()
+    incfiles['DH_VAR_T'].body = DH.dfDHT.to_string()
+
+    for file in incfiles.keys():
+        incfiles[file].save()
+
+    return DH
+
+#%% ------------------------------- ###
+###          2. VRE Data            ###
+### ------------------------------- ###
+
+
+
+#%% ------------------------------- ###
+###        X. Generate Data         ###
+### ------------------------------- ###
+
+if __name__ == '__main__':
+
+    ### X.1 Load the desired spatial resolution
+    choice = 'nuts3'
+    the_index, areas, c = prepared_geofiles(choice)
+
+    ### X.2 Generate Data
+    # DH = generate_DH_data(areas)
     
-incfiles['DH'].body = DH.dfDH.to_string()
-incfiles['DH_VAR_T'].body = DH.dfDHT.to_string()
-
-
-for file in incfiles.keys():
-    incfiles[file].save()
+    
