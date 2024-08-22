a061d1c79ac6df439955bce07fd8c06262a11f84
Mathias157 <mathiasros@gmail.com>
2024-08-22 15:09:25 +0200

    Downloaded energinet municipality electricity profiles and started new module for processing this

---
+++ Raw Data Processing/Modules/createDE.py
@@ -0,0 +1,30 @@
+"""
+Created on 22.08.2024
+
+@author: Mathias Berg Rosendal, PhD Student at DTU Management (Energy Economics & Modelling)
+"""
+#%% ------------------------------- ###
+###        0. Script Settings       ###
+### ------------------------------- ###
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+style = 'report'
+
+if style == 'report':
+    plt.style.use('default')
+    fc = 'white'
+elif style == 'ppt':
+    plt.style.use('dark_background')
+    fc = 'none'
+
+#%% ------------------------------- ###
+###        1. 
+### ------------------------------- ###
+
+
+f = pd.read_csv(r'Data\Timeseries\ElConsumptionEnerginet2023.csv', sep=';', decimal=',')
+
+
