59f9383e8b284068269ec7e4bf924430621eee86
Mathias157 <mathiasros@gmail.com>
2024-08-22 15:10:30 +0200

    Some documentation

--- Raw Data Processing/Modules/createDE.py
+++ Raw Data Processing/Modules/createDE.py
@@ -1,4 +1,8 @@
 """
+Create electricity demand profiles for Denmark
+Use Energinet data for municipalities:
+https://www.energidataservice.dk/tso-electricity/consumptionindustry
+
 Created on 22.08.2024
 
 @author: Mathias Berg Rosendal, PhD Student at DTU Management (Energy Economics & Modelling)
