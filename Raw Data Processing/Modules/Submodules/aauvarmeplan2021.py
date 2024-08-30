15457245f7381010c9c1665184875b7380bce571
Mathias157 <mathiasros@gmail.com>
2024-08-30 19:19:17 +0200

    A suggestion on preliminary assumption and preparing to use industry shapefiles

--- Raw Data Processing/Modules/Submodules/aauvarmeplan2021.py
+++ Raw Data Processing/Modules/Submodules/aauvarmeplan2021.py
@@ -5,6 +5,19 @@ Aalborg dataset, can be used to get individual and district heat gross supply:
 https://vbn.aau.dk/da/datasets/kommunepakker-varmeplan-danmark-2021
 - Note that heat demand should be combined wrt. the s1-s5 scenarios (sums to 53.95 TWh without heat efficiency investments)
 
+Assumption for distribution of low-, mid- and high temperatures should probably come from:
+Viegand Maagø and Energistyrelsen. ‘Kortlægning Af Energiforbrug Og Opgørelse Af Energisparepotentialer i Produktionserhvervene’, 2022.
+See page 8:
+“
+- Omkring 45% af forbruget ligger ved temperaturer under 100 ºC – totalt 26.798 TJ/år 
+- Godt 40% ligger ved temperaturer over 200 ºC – totalt 23.774 TJ/år 
+- Resten, altså ca. 15%, ligger i intervallet 100 – 200 ºC eller derover
+”, Viegand Maagø and Energistyrelsen, 2022, p. 8
+
+One simple, first assumption could be to say that 40-55% is high temperature, 
+and use that amount of TJ to distribute the sum of TJ in the "over 80C" category
+of Aalborg data into high and middle temperature (middle temperature is the remaining over 80C)  
+
 Python package requirements:
 - xlrd 
 
@@ -46,18 +59,23 @@ class VPDK21:
                          'HøjeTaastrup' : 'Høje Taastrup',
                          'Vesthimmerlands' : 'Vesthimmerland'}
         
-        # Read varmeplan data
+        # Loop through varmeplan data
         for file in files:
+            ## Get summary data
             f = pd.read_excel(f'Data/AAU Kommuneplan/{file}/{file}_opsummering.xls')
 
             if file in correct_names.keys():
                 file = correct_names[file]
             
-            # Put municipality in index as multiindex
+            ## Get industry surplus heat supply
+            f2 = gpd.read_file(f'Data/AAU Kommuneplan/{file}/gis_data/{file}_industri.shp')
+            print(f2.to_string())
+            
+            ## Put municipality in index as multiindex
             f.index = pd.MultiIndex.from_product([[file], np.arange(len(f))],
                                                     names=['municipality', 'original'])
             
-            # Categorise District Heating and Individual Heating
+            ## Categorise District Heating and Individual Heating
             f['user'] = (
                 f.Forsyning.str
                 .replace('Andet', 'individual')
@@ -70,10 +88,13 @@ class VPDK21:
                 .astype('category')
             )
             
-            # Store year of data collected
+            ## Store Industry
+
+            
+            ## Store year of data collected
             f['year'] = 2019
             
-            # Sum to scenario
+            ## Sum to scenario
             f = f.pivot_table(index=['year', 'municipality', 'user'], values=scenario, 
                               aggfunc=lambda x: np.sum(x)*1e3) # To MWh
             
@@ -98,5 +119,5 @@ if __name__ == '__main__':
         geo.plot(
                     column=user,
                     cmap='coolwarm',  # Add the cmap parameter here
-                    legend=True  # Add the legend parameter here
+                    legend=True       # Add the legend parameter here
                 ).set_title(user + ' Heat Demand (MWh)')
