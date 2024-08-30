60aaddcca8ceecd030052bfbfe10fe6a727653cc
Mathias157 <mathiasros@gmail.com>
2024-08-30 22:14:48 +0200

    Made key for industrial process temperatures

--- Raw Data Processing/Modules/Submodules/aauvarmeplan2021.py
+++ Raw Data Processing/Modules/Submodules/aauvarmeplan2021.py
@@ -17,6 +17,18 @@ See page 8:
 One simple, first assumption could be to say that 40-55% is high temperature, 
 and use that amount of TJ to distribute the sum of TJ in the "over 80C" category
 of Aalborg data into high and middle temperature (middle temperature is the remaining over 80C)  
+- E.g.: 
+- 59496 TJ/år in total
+- 32723 TJ/år for >100 C (55%)
+AAU data:
+GJ_over_80    17.1217 %       3369.050427 TJ
+GJ_60_80C     12.1648 %       2393.687324 TJ
+GJ_under_6    70.7135 %       13914.38463 TJ
+..doesn't really fit - maybe the assumption that surplus heat above 80C comes from high, 60-80C from mid and below 60C from low is good enough?
+for that to be true:
+- 45% is low => 25.71% left from GJ under 60C
+- 15% is mid => 10.71% left from GJ under 60C
+- 40% is high => 10.71% of < 60C, 12.1648% of GJ_60_80C and 17.1217%
 
 Python package requirements:
 - xlrd 
@@ -53,24 +65,25 @@ class VPDK21:
     def __init__(self, scenario: str = 'SUM_GWh_uden_besp') -> None:    
         files = os.listdir('Data/AAU Kommuneplan')
         self.DH = pd.DataFrame()
+        self.IND = pd.DataFrame()
         
         # Correcting Municipal Names
         correct_names = {'Aarhus' : 'Århus',
-                         'HøjeTaastrup' : 'Høje Taastrup',
+                         'Høje_Taastrup' : 'Høje Taastrup',
                          'Vesthimmerlands' : 'Vesthimmerland'}
         
         # Loop through varmeplan data
         for file in files:
             ## Get summary data
             f = pd.read_excel(f'Data/AAU Kommuneplan/{file}/{file}_opsummering.xls')
-
-            if file in correct_names.keys():
-                file = correct_names[file]
             
             ## Get industry surplus heat supply
             f2 = gpd.read_file(f'Data/AAU Kommuneplan/{file}/gis_data/{file}_industri.shp')
-            print(f2.to_string())
             
+            if file in correct_names.keys():
+                file = correct_names[file]
+                
+                
             ## Put municipality in index as multiindex
             f.index = pd.MultiIndex.from_product([[file], np.arange(len(f))],
                                                     names=['municipality', 'original'])
@@ -89,6 +102,12 @@ class VPDK21:
             )
             
             ## Store Industry
+            f2 = pd.DataFrame(
+                f2[['GJ_over_80', 'GJ_60_80C', 'GJ_under_6']].sum()
+            ).T
+            f2.index = [file]
+            f2.index.name = 'municipality'
+            self.IND = pd.concat((self.IND, f2))
 
             
             ## Store year of data collected
@@ -100,24 +119,51 @@ class VPDK21:
             
             self.DH = pd.concat((self.DH, f))
                 
+        # Assign heat temperatures, based on surplus heat (read assumption in script description)
+        self.IND['industry_phl'] = 0.45*self.IND.GJ_under_6 
+        self.IND['industry_phm'] = 0.15*self.IND.GJ_under_6
+        self.IND['industry_phh'] = (0.1071*self.IND.GJ_under_6 + self.IND.GJ_60_80C + self.IND.GJ_over_80) 
+        
+        ## Normalise to total surplus heat
+        self.IND = self.IND.drop(columns=['GJ_over_80', 'GJ_60_80C', 'GJ_under_6'])
+        total_surplus_heat = self.IND.sum().sum()
+        self.IND['industry_phl'] = self.IND['industry_phl'] /  total_surplus_heat
+        self.IND['industry_phm'] = self.IND['industry_phm'] /  total_surplus_heat
+        self.IND['industry_phh'] = self.IND['industry_phh'] /  total_surplus_heat
+        temp = pd.DataFrame(index=pd.MultiIndex.from_product(([2019], self.IND.index, ['industry_phl',
+                                                                              'industry_phm',
+                                                                              'industry_phh'])))
+        temp.index.names = ['year', 'municipality', 'user']
+        temp.loc[(2019, slice(None), 'industry_phl'), 'heat_demand_normalised'] = self.IND.industry_phl.values
+        temp.loc[(2019, slice(None), 'industry_phm'), 'heat_demand_normalised'] = self.IND.industry_phm.values
+        temp.loc[(2019, slice(None), 'industry_phh'), 'heat_demand_normalised'] = self.IND.industry_phh.values
+                
         # Convert to xarray
         self.DH = self.DH.to_xarray()
         self.DH = self.DH.rename({scenario : 'heat_demand_mwh'})
-        
+        self.IND = temp.to_xarray()
        
 
 if __name__ == '__main__':
     data = DataContainer()
     VP = VPDK21()
     data.muni = data.muni.merge(VP.DH)
+    data.muni = data.muni.merge(VP.IND)
 
     # Plotting geometry
     geo = data.get_polygons('muni')
     for user in data.muni.user.data:
-        geo[user] = data.muni.heat_demand_mwh.sel(user=user,
-                                                  year=2019).data
+        if user == 'district_heating' or user == 'individual':
+            geo[user] = data.muni.heat_demand_mwh.sel(user=user,
+                                                    year=2019).data
+            unit = 'MWh'
+        else:
+            geo[user] = data.muni.heat_demand_normalised.sel(user=user,
+                                                    year=2019).data
+            unit = 'normalised'
+        
         geo.plot(
                     column=user,
                     cmap='coolwarm',  # Add the cmap parameter here
                     legend=True       # Add the legend parameter here
-                ).set_title(user + ' Heat Demand (MWh)')
+                ).set_title(user + f' Heat Demand ({unit})')
