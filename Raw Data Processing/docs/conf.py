33394eb0c44dfa42d880a4d6d0546b36f03ff347
Mathias157 <mathiasros@gmail.com>
2024-09-20 16:49:21 +0200

    Added docs

---
+++ Raw Data Processing/docs/conf.py
@@ -0,0 +1,32 @@
+project = "Balmorel Pre-Processing Framework"
+copyright = "2024, Mathias Berg Rosendal"
+author = "Mathias Berg Rosendal"
+release = "0.0.1"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv", ".venv/**"]
+
+conf_py_path = "Raw Data Processing/docs/"  # with leading and trailing slash
+
+html_static_path = ["css"]
+
+# General configurations
+extensions = [
+    "myst_parser",  # in order to use markdown
+    "autoapi.extension",  # in order to use markdown
+    "sphinx_copybutton",
+]
+
+# search this directory for Python files
+autoapi_dirs = ["Raw Data Processing"]
+
+# ignore this file when generating API documentation
+autoapi_ignore = ["*/conf.py"]
+
+myst_enable_extensions = [
+    "colon_fence",  # ::: can be used instead of ``` for better rendering    
+]
+
+html_theme = "sphinx_rtd_theme"
+
+def setup(app):
+    app.add_css_file('css_options.css')  # may also be an URL
