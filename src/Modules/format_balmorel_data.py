06d21e0f2bd5081a5c2090a3877c06ea9a1081b4
Mathias157 <mathiasros@gmail.com>
2024-09-24 12:22:57 +0200

    Won't load if already loaded or overwrite not specified

--- src/Modules/format_balmorel_data.py
+++ src/Modules/format_balmorel_data.py
@@ -12,21 +12,27 @@ Created on 24.09.2024
 
 from pybalmorel import Balmorel
 import click
+import os
 
 #%% ------------------------------- ###
 ###        1. 
 ### ------------------------------- ###
 
-def get_all_data(balmorel_model_path: str, scenario: str):
+def get_all_data(balmorel_model_path: str, scenario: str, load_again: bool = False):
     balm = Balmorel(balmorel_model_path)
-    print('Loaded scenarios: %s'%balm.scenarios)
-    balm.load_incfiles(scenario)
+    
+    if not('muni_input_data.gdx' in os.listdir(os.path.join(balm.path, 'muni/model'))) or load_again == True:      
+        print('\nLoading results into %s_input_data.gdx...\n'%scenario)
+        balm.load_incfiles(scenario)
+    else:
+        print('\n%s_input_data.gdx already loaded!\n'%scenario)
     
 @click.command()
 @click.option("--model-path", type=str, required=True, help="Path of the Balmorel model")
 @click.option("--scenario", type=str, required=True, help="Scenario to load results from")
-def main(model_path: str, scenario: str):
-    get_all_data(model_path, scenario)
+@click.option("--load-again", type=bool, required=False, help="Load scenario results again and overwrite previously loaded .gdx?")
+def main(model_path: str, scenario: str, load_again: bool = False):
+    get_all_data(model_path, scenario, load_again)
         
 if __name__ == '__main__':
     main()
