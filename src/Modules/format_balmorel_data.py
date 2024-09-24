196969c97cfae990cc6011af92b37b1f7a59d036
Mathias157 <mathiasros@gmail.com>
2024-09-24 22:34:40 +0200

    Added VRE timeseries (note that a lot of code is duplicated in format_balmorel_data.... I didn't want to make it pretty because it was a quick fix)

--- src/Modules/format_balmorel_data.py
+++ src/Modules/format_balmorel_data.py
@@ -14,6 +14,7 @@ from gams import GamsWorkspace
 from pybalmorel import Balmorel
 from pybalmorel.utils import symbol_to_df
 from Submodules.utils import df_set_to_dictionary, combine_dicts
+import geopandas as gpd
 import pickle
 from geofiles import prepared_geofiles
 from typing import Tuple
@@ -36,7 +37,10 @@ def store_balmorel_input(symbol: str,
     balm = Balmorel(balmorel_model_path)
     
     # Check if the symbol.gzip exists
-    if not('%s.gzip'%symbol in os.listdir('Data/BalmorelData')):
+    if '%s.gzip'%symbol in os.listdir('Data/BalmorelData'):
+        print('\n%s.gzip already exists\n'%symbol)
+        f = pd.read_parquet('Data/BalmorelData/%s.gzip'%symbol)
+    else:
         # Check Balmorel input has been loaded
         if not('muni_input_data.gdx' in os.listdir(os.path.join(balm.path, 'muni', 'model'))) or load_again == True:      
             print('\nLoading results into %s_input_data.gdx...\n'%scenario)
@@ -54,13 +58,25 @@ def store_balmorel_input(symbol: str,
         if filter_func != None:
             f = filter_func(f)
         f.to_parquet('Data/BalmorelData/%s.gzip'%symbol)
-    
-    else:
-        print('\n%s.gzip already exists\n'%symbol)
-        f = pd.read_parquet('Data/BalmorelData/%s.gzip'%symbol)
         
     return f
 
+def join_to_gpd(df: pd.DataFrame,
+                left_col: str,
+                gpd: gpd.GeoDataFrame,
+                right_col: str,
+                new_col: list, 
+                suffix: Tuple[None, str] = None):
+    if suffix != None:
+        df[left_col] = df.A.str.replace(suffix, '')
+    df.index = df[left_col]
+    df = df.join(gpd[right_col])
+    df.columns = new_col
+    df.index = range(len(df))
+    if suffix != None:
+        df[left_col] = df[left_col] + suffix
+    
+    return df 
 
 #%% ------------------------------- ###
 ###            1. Main              ###
@@ -71,37 +87,30 @@ def store_balmorel_input(symbol: str,
 @click.option("--scenario", type=str, required=True, help="Scenario to load results from")
 @click.option("--load-again", type=bool, required=False, help="Load scenario results again and overwrite previously loaded .gdx?")
 def main(model_path: str, scenario: str, load_again: bool = False):
-    # 1. Load DH_VAR_T
-    df = store_balmorel_input('DH_VAR_T', ['A', 'DHUSER', 'S', 'T', 'Value'], 
-                         model_path, scenario, load_again, 
-                         lambda x: x.loc[x.A.str.contains('DK_')].query("DHUSER == 'RESH'"))
-    
-    # Check output
-    f = pd.read_parquet('Data/BalmorelData/DH_VAR_T.gzip')
-    
-    # Convert municipal codes to names
+    # 1.1 Load Geodataframe with municipal codes and names
     ind, mun, country = prepared_geofiles('DK Municipalities')
     
-    ## Remove _A suffix and join name
-    df['A'] = df.A.str.replace('_A', '')
-    df.index = df.A
-    df = df.join(mun.NAME_2)
-    df.columns = ['A_old', 'DHUSER', 'S', 'T', 'Value', 'A']
-    df.index = range(len(df))
-    
-    ## Replace æ, ø, å
-    df['A'] = (
-        df.A
+    ## Convert æ, ø, å
+    mun['NAME_2'] = (
+        mun.NAME_2
         .str.replace('Æ', 'Ae')
         .str.replace('Ø', 'Oe')
         .str.replace('Å', 'Aa')
         .str.replace('æ', 'ae')
         .str.replace('ø', 'oe')
-        .str.replace('å', 'aa')
-        .add('_A')
+        .str.replace('å', 'aa') 
     )
     
-    ## 1.1 Save DH_VAR_T.inc
+    # 1.2 Load DH_VAR_T
+    df = store_balmorel_input('DH_VAR_T', ['A', 'DHUSER', 'S', 'T', 'Value'], 
+                         model_path, scenario, load_again, 
+                         lambda x: x.loc[x.A.str.contains('DK_')].query("DHUSER == 'RESH'"))
+    
+    ## Join municipal codes ('A') to names ('NAME_2')
+    df = join_to_gpd(df, 'A', mun, 'NAME_2', 
+                     ['A_old', 'DHUSER', 'S', 'T', 'Value', 'A'], '_A')
+        
+    ### 1.2.1 Save DH_VAR_T.inc
     incfile = IncFile(name='DH_VAR_T', path='Output',
                       prefix='\n'.join([
                             "PARAMETER DH_VAR_T(AAA,DHUSER,SSS,TTT) 'Variation in heat demand';",
@@ -120,7 +129,7 @@ def main(model_path: str, scenario: str, load_again: bool = False):
                          ['A', 'DHUSER'])
     incfile.save()
     
-    ## 1.2 Save INDIVUSERS_DH_VAR_T
+    ### 1.2.2 Save INDIVUSERS_DH_VAR_T
     df['A'] = df.A.str.replace('_A', '_IDVU-SPACEHEAT')
     df['DHUSER'] = 'RESIDENTIAL'
     incfile = IncFile(name='INDIVUSERS_DH_VAR_T', path='Output',
@@ -139,7 +148,107 @@ def main(model_path: str, scenario: str, load_again: bool = False):
     incfile.body_prepare(['S', 'T'],
                          ['A', 'DHUSER'])
     incfile.save()
-
+    
+    # 1.3 Load and Save WND_VAR_T
+    df = store_balmorel_input('WND_VAR_T', ['A', 'S', 'T', 'Value'], 
+                         model_path, scenario, load_again, 
+                         lambda x: x.loc[x.A.str.contains('DK_')])
+    
+    ## Join municipal codes ('A') to names ('NAME_2')
+    df = join_to_gpd(df, 'A', mun, 'NAME_2', 
+                     ['A_old', 'S', 'T', 'Value', 'A'], '_A')
+    
+    incfile = IncFile(name='WND_VAR_T', path='Output',
+                prefix='\n'.join([
+                    "TABLE WND_VAR_T1(SSS,TTT,AAA) 'Variation of the wind generation';",
+                    ""
+                ]),
+                body=df,
+                suffix='\n'.join([
+                    "",
+                    ";",
+                    "WND_VAR_T(AAA,SSS,TTT) = WND_VAR_T1(SSS,TTT,AAA);",
+                    "WND_VAR_T1(SSS,TTT,AAA) = 0;",
+                ]))
+    incfile.body_prepare(['S', 'T'],
+                         ['A'])
+    incfile.save()
+    
+    
+    # 1.4 Load and Save SOLE_VAR_T
+    df = store_balmorel_input('SOLE_VAR_T', ['A', 'S', 'T', 'Value'], 
+                         model_path, scenario, load_again, 
+                         lambda x: x.loc[x.A.str.contains('DK_')])
+    
+    ## Join municipal codes ('A') to names ('NAME_2')
+    df = join_to_gpd(df, 'A', mun, 'NAME_2', 
+                     ['A_old', 'S', 'T', 'Value', 'A'], '_A')
+    
+    incfile = IncFile(name='SOLE_VAR_T', path='Output',
+                prefix='\n'.join([
+                    "TABLE SOLE_VAR_T1(SSS,TTT,AAA) 'Variation of the solar generation';",
+                    ""
+                ]),
+                body=df,
+                suffix='\n'.join([
+                    "",
+                    ";",
+                    "SOLE_VAR_T(AAA,SSS,TTT) = SOLE_VAR_T1(SSS,TTT,AAA);",
+                    "SOLE_VAR_T1(SSS,TTT,AAA) = 0;",
+                ]))
+    incfile.body_prepare(['S', 'T'],
+                         ['A'])
+    incfile.save()
+    
+    # 1.5 Load and save WNDFLH
+    df = store_balmorel_input('WNDFLH', ['A', 'Value'], 
+                         model_path, scenario, load_again, 
+                         lambda x: x.loc[x.A.str.contains('DK_')])
+    
+    ## Join municipal codes ('A') to names ('NAME_2')
+    df = join_to_gpd(df, 'A', mun, 'NAME_2', 
+                     ['A_old', 'Value', 'A'], '_A')
+    
+    incfile = IncFile(name='WNDFLH', path='Output',
+                prefix='\n'.join([
+                    "PARAMETER WNDFLH(AAA)  'Full load hours for solar power'",
+                    "/"  
+                ]),
+                body=df,
+                suffix='\n'.join([
+                    "",
+                    "/",
+                    ";",
+                ]))
+    incfile.body = incfile.body.pivot_table(index='A', values='Value', aggfunc='sum')
+    incfile.body.index.name = ''
+    incfile.body.columns = ['']
+    incfile.save()
+    
+    # 1.6 Load and save SOLEFLH
+    df = store_balmorel_input('SOLEFLH', ['A', 'Value'], 
+                         model_path, scenario, load_again, 
+                         lambda x: x.loc[x.A.str.contains('DK_')])
+    
+    ## Join municipal codes ('A') to names ('NAME_2')
+    df = join_to_gpd(df, 'A', mun, 'NAME_2', 
+                     ['A_old', 'Value', 'A'], '_A')
+    
+    incfile = IncFile(name='SOLEFLH', path='Output',
+                prefix='\n'.join([
+                    "PARAMETER SOLEFLH(AAA)  'Full load hours for solar power'",
+                    "/"  
+                ]),
+                body=df,
+                suffix='\n'.join([
+                    "",
+                    "/",
+                    ";",
+                ]))
+    incfile.body = incfile.body.pivot_table(index='A', values='Value', aggfunc='sum')
+    incfile.body.index.name = ''
+    incfile.body.columns = ['']
+    incfile.save()
     
 if __name__ == '__main__':
     main()
