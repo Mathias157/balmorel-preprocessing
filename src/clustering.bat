4ab46994e2b2892192ed5e7dbdd162609eea14f0
Mathias157 <mathiasros@gmail.com>
2024-09-30 21:48:05 +0200

    Prepared structure in clustering snakefile

--- src/clustering.bat
+++ src/clustering.bat
@@ -1,6 +1,6 @@
 @REM Run workflow
-snakemake -s .workflow/clustering
+snakemake -s clustering
 
 @REM Make DAG plot
-snakemake -s .workflow/clustering --dag > analysis/clustering_dot_source.txt
+snakemake -s clustering --dag > analysis/clustering_dot_source.txt
 python analysis/plot_dag.py --workflow=clustering --view=false
--- src/preprocessing.bat
+++ src/preprocessing.bat
@@ -1,6 +1,6 @@
 @REM Run workflow
-snakemake -s .workflow/preprocessing
+snakemake -s preprocessing
 
 @REM Make DAG plot
-snakemake -s .workflow/preprocessing --dag > analysis/preprocessing_dot_source.txt
+snakemake -s preprocessing --dag > analysis/preprocessing_dot_source.txt
 python analysis/plot_dag.py --workflow=preprocessing --view=false
